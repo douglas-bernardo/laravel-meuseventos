fb4c0d942a4fa047dbdae2c16aa9f0a8c65c37f2
Douglas Bernardo <jkdouglas21@gmail.com>
2022-02-04 22:13:49 -0300

    First commit project

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->sentence;
+        return [
+            'title' => $name,
+            'description' => $this->faker->words(7, true),
+            'body' => $this->faker->paragraph,
+            'start_event' => now(),
+            'slug' => Str::slug($name),
+        ];
+    }
+}
